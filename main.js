5f7567b0d1022fd7b0c14cb68c52137b85ede9cc
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-07-23 17:21:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+mx=0;
+my=0
+
+function preload() {
+img=loadImage('mumbustache.png');
+}
+function setup(){
+    Canvas=createCanvas(375,300);
+    Canvas.center();
+    //cam=createCapture(VIDEO);
+    cam.size(375,300);
+    cam.hide();
+    net=ml5.poseNet(cam,modelloaded);
+    net.on('pose',gotposes);
+}
+function draw(){
+    image(cam,0,0,375,300);
+   image(img,mx,my,30,30);
+}
+function ohsnap(){
+    save("mustache.png");
+}
+function modelloaded(){
+    console.log("woohoo !!! your model is loaded.");
+}
+function gotposes(results){
+    
+    if(results.length>0){
+        console.log(results);
+        console.log("nose x="+results[0].pose.nose.x);
+        console.log("nose y="+results[0].pose.nose.y);
+        mx=results[0].pose.nose.x-20;
+        my=results[0].pose.nose.y+30; 
+
+    }
+
+}
